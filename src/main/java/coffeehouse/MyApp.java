0af8cf2506c3d854a23b90720f402959cd8549b5
SamBugatti <213041@astanait.edu.kz>
2022-02-23 21:58:09 +0600

    AdminMenu and Tables

--- src/main/java/coffeehouse/MyApp.java
+++ src/main/java/coffeehouse/MyApp.java
@@ -11,7 +11,7 @@ public class MyApp {
         while (choice != 0){
             System.out.println("Show menu : please enter 1 that choice this command ");
             System.out.println("Take order : please enter 2 that choice this command "); // тут будут дальше команды для оплаты заказа и номер заказа !!
-            System.out.println("Close : please enter 2 that choice this command ");
+            System.out.println("Close : please enter 3 that choice this command ");
             System.out.println(" staff only employeers : 00");
             choice = scanner.nextInt();
             switch (choice){
--- src/main/java/coffeehouse/Organization.java
+++ src/main/java/coffeehouse/Organization.java
@@ -2,9 +2,7 @@ package coffeehouse;
 
 import coffeehouse.Data.DB;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Organization {
     DB db  = new DB();
@@ -12,18 +10,37 @@ public class Organization {
     public void ShowMenu() throws SQLException, ClassNotFoundException {
         Connection connection = db.DataBaseConn();
         try {
-            String sql = "insert into menu( name , price ) values (? , ?)";
+            Statement statement = connection.createStatement();
             String sql2  = "select * from menu";
             PreparedStatement preparedStatement = connection.prepareStatement(sql2);
-//            preparedStatement.setString(1 , "sssssss");
-//            preparedStatement.setInt(2 , 500);
-            preparedStatement.executeQuery();
-
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()){
+                System.out.println( resultSet.getString(1) +" " + resultSet.getString(2) + " " + resultSet.getString(3));
+            }
 
         }
         catch (Exception e){
             e.printStackTrace();
         }
 
+    }
+
+    public void EditMenu(){
+        Connection connection = db.DataBaseConn();
+        try {
+            Statement statement = connection.createStatement();
+
+            PreparedStatement preparedStatement = connection.prepareStatement(sql2);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()){
+                System.out.println( resultSet.getString(1) +" " + resultSet.getString(2) + " " + resultSet.getString(3));
+            }
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
     }
 }
